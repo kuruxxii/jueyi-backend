e2ba5e9f4599b7dbd77fb6e3094df78840c4ef48
kuruxxii <zzzihanlin@gmail.com>
2024-05-26 18:51:22 +0800

    fix bug
    return res.status, etc

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { login, verify } from "../controllers/auth";
+import { login, verify, status } from "../controllers/auth";
 
 const router = Router();
 
 router.post("/login", login);
 router.get("/verify", verify);
+router.get("/status", status);
 
 export default router;
