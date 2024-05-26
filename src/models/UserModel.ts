08673a1c132a3ea17fe686c6a09eca6fca52b38d
kuruxxii <zzzihanlin@gmail.com>
2024-05-26 10:52:30 +0800

    half way email auth

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -4,7 +4,7 @@ import type { User } from "../lib/definitions";
 
 const userSchema = new Schema<User>({
   email: { type: String, required: true, unique: true },
-  number: { type: String, required: true },
+  number: { type: String, required: true, unique: true },
   subscription: {
     isActive: { type: Boolean, required: true },
     startDate: { type: Date, required: true },
