7c8bcc928cf99c722d81b268b30c136e755f6805
kuruxxii <zzzihanlin@gmail.com>
2024-05-30 09:20:38 +0800

    getRecommendations

--- src/routes/articles.ts
+++ src/routes/articles.ts
@@ -3,12 +3,14 @@ import {
   getAnArticle,
   getFilteredArticlePreviews,
   getFilteredArticlePreviewTotalPages,
+  getRecommendations,
 } from "../controllers/articles";
 
 const router = Router();
 
 router.get("/", getFilteredArticlePreviews);
 router.get("/pages", getFilteredArticlePreviewTotalPages);
+router.get("/recommendations", getRecommendations);
 router.get("/:slug", getAnArticle);
 
 export default router;
