512aa6db3fba5927f6574067b7a9147ece579b48
kuruxxii <zzzihanlin@gmail.com>
2024-05-31 11:09:24 +0800

    logout

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { login, verify, status } from "../controllers/auth";
+import { login, verify, status, logout } from "../controllers/auth";
 
 const router = Router();
 
 router.post("/login", login);
 router.get("/verify", verify);
 router.get("/status", status);
+router.get("/logout", logout);
 
 export default router;
