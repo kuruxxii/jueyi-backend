cd437588896283d3e60c7cf1c108477f968588e8
kuruxxii <zzzihanlin@gmail.com>
2024-06-02 11:15:03 +0800

    check isSubscriptinActive

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -2,14 +2,29 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 import type { User } from "../lib/definitions";
 
-const userSchema = new Schema<User>({
-  email: { type: String, required: true, unique: true },
-  number: { type: String, required: true, unique: true },
-  subscription: {
-    isActive: { type: Boolean, required: true },
-    startDate: { type: Date, required: true },
-    endDate: { type: Date, required: true },
+// interface UserDocument extends Document, User {
+//   isSubscriptionActive: () => boolean;
+// }
+
+type UserMethods = { isSubscriptionActive: () => boolean };
+
+type UserModel = User & UserMethods;
+
+const userSchema = new Schema<UserModel>(
+  {
+    email: { type: String, required: true, unique: true, immutable: true },
+    number: { type: String, required: true, unique: true },
+    subscription: {
+      startDate: { type: Date, required: true },
+      endDate: { type: Date, required: true },
+    },
   },
-});
+  { timestamps: true }
+);
+
+userSchema.methods.isSubscriptionActive = function () {
+  const now = new Date(); // Get the current date and time
+  return now <= this.subscription.endDate;
+};
 
 export const UserModel = model("User", userSchema);
