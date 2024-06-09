94f4d1fd6fe93d4bf33dca01f3a8b654631075a1
kuruxxii <zzzihanlin@gmail.com>
2024-06-09 16:47:08 +0800

    add rate limiter

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,17 @@ import authRouter from "./routes/auth";
 import articlesRouter from "./routes/articles";
 import journalsRouter from "./routes/journals";
 import newsRouter from "./routes/news";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
+
+// Configure the rate limiter
+const authRateLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 5, // Limit each IP to 5 auth requests per `window` (here, per 1 minute)
+  message: JSON.stringify({ msg: "您已达到请求限制，请稍后再试。" }), // Custom message when rate limit is exceeded
+  headers: true, // Include rate limit info in the `RateLimit-*` headers
+});
 
 app.use(morgan("combined"));
 app.use(
@@ -27,7 +36,7 @@ app.use(cookieParser());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
-app.use("/api/auth", authRouter);
+app.use("/api/auth", authRateLimiter, authRouter);
 app.use("/api/articles", ensureAuthenticated, articlesRouter);
 app.use("/api/journals", ensureAuthenticated, journalsRouter);
 app.use("/api/news", ensureAuthenticated, newsRouter);
